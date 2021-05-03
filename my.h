f1f6ff77e99532504f0a8e4062466965c67d3d02
Mehul Minat <72225679+mehulminat@users.noreply.github.com>
2021-05-03 19:48:01 +0530

    Userdefined header file

---
+++ my.h
@@ -0,0 +1,10 @@
+void mehul()
+{
+        
+        std::cout << "\n\n";
+        std::cout << "\t*********************************\n";
+        std::cout << "\t*\t Made By : Mehul\t*\n";
+        std::cout << "\t*-------------------------------*\n";
+        std::cout << "\t*\t Id No.:20DCS049\t*\n";
+        std::cout << "\t*********************************\n";
+}
